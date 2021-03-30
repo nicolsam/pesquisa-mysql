5e107c0345b0e4708fc9c8af1ab78bcfdbecae03
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-03-29 21:23:59 -0300

    Setting Class Model Artigo

--- public/model/ArtigoModel.php
+++ public/model/ArtigoModel.php
@@ -1,6 +1,29 @@
 <?php
     class Artigo {
+        private $id;
+        private $titulo;
+        private $conteudo;
 
+        public function setId($id) {
+            $this->id = $id;
+        }
+        public function getId() {
+            return $this->id;
+        }
+
+        public function setTitulo($titulo) {
+            $this->titulo = $titulo;
+        }
+        public function getTitulo() {
+            return $this->titulo;
+        }
+
+        public function setConteudo($conteudo) {
+            $this->conteudo = $conteudo;
+        }
+        public function getConteudo() {
+            return $this->conteudo;
+        }
     }
 
 
